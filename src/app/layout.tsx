486aa7500b46ea1c6270abc6beb6617ea5ec9565
XGM <109265751+xgmxgm@users.noreply.github.com>
2024-10-10 19:26:16 +0600

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
+	src: './Fonts/GeistVF.woff',
 	variable: '--font-geist-sans',
 	weight: '100 900',
 })
 const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
+	src: './Fonts/GeistMonoVF.woff',
 	variable: '--font-geist-mono',
 	weight: '100 900',
 })
